90aa85ab9b369e3678f6886ed1cec4cd1b63db96
dnfvnsxn <dnfvnsxn@naver.com>
2020-01-21 16:52:11 +0900

    filter button

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 var app = angular.module("todo", []);
 
 app.controller("TodoCtrl", [
-    "$scope",
-    function($scope) {
-        $scope.todos = [
-            {
-                id: 0,
-                title: "t1",
-                completed: false,
-                createdAt: Date.now()
-            },
-            {
-                id: 1,
-                title: "t2",
-                completed: false,
-                createdAt: Date.now()
-            },
-            {
-                id: 2,
-                title: "t3",
-                completed: true,
-                createdAt: Date.now()
-            }
-        ];
+  "$scope",
+  function($scope) {
+    $scope.todos = [
+      {
+        id: 0,
+        title: "t1",
+        completed: false,
+        createdAt: Date.now()
+      },
+      {
+        id: 1,
+        title: "t2",
+        completed: false,
+        createdAt: Date.now()
+      },
+      {
+        id: 2,
+        title: "t3",
+        completed: true,
+        createdAt: Date.now()
+      }
+    ];
 
-        $scope.remove = function(todo) {
-            var idx = $scope.todos.findIndex(function(item) {
-                return item.id === todo.id;
-            });
+    $scope.remove = function(todo) {
+      var idx = $scope.todos.findIndex(function(item) {
+        return item.id === todo.id;
+      });
 
-            if (idx > -1) {
-                $scope.todos.splice(idx, 1);
-            }
-        };
-    }
+      if (idx > -1) {
+        $scope.todos.splice(idx, 1);
+      }
+    };
+  }
 ]);

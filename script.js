f805a715c186995b56f87e520d1d9a291a7800dc
dnfvnsxn <dnfvnsxn@naver.com>
2020-01-21 15:33:31 +0900

    ngClick

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-(function() {
-    var app = angular.module("todo", []);
+var app = angular.module("todo", []);
 
-    app.controller("TodoCtrl", [
-        "$scope",
-        function($scope) {
-            $scope.todos = [
-                {
-                    title: "t1",
-                    completed: false,
-                    createdAt: Date.now()
-                },
-                {
-                    title: "t2",
-                    completed: false,
-                    createdAt: Date.now()
-                },
-                {
-                    title: "t3",
-                    completed: true,
-                    createdAt: Date.now()
-                }
-            ];
-        }
-    ]);
-})();
+app.controller("TodoCtrl", [
+    "$scope",
+    function($scope) {
+        $scope.todos = [
+            {
+                id: 0,
+                title: "t1",
+                completed: false,
+                createdAt: Date.now()
+            },
+            {
+                id: 1,
+                title: "t2",
+                completed: false,
+                createdAt: Date.now()
+            },
+            {
+                id: 2,
+                title: "t3",
+                completed: true,
+                createdAt: Date.now()
+            }
+        ];
+
+        $scope.remove = function(todo) {
+            var idx = $scope.todos.findIndex(function(item) {
+                return item.id === todo.id;
+            });
+
+            if (idx > -1) {
+                $scope.todos.splice(idx, 1);
+            }
+        };
+    }
+]);
